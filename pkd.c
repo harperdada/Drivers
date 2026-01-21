e795f9316438ad1168f15e440f1f0c53fd85a6f3
Prav Dubey <prav@pravdubey1.com>
2026-01-21 08:57:51 -0800

    Added struct file_operations

--- pkd.c
+++ pkd.c
@@ -28,3 +28,20 @@ ssize_t pkd_write(struct file *, const char __user *, size_t, loff_t *);
 #define SUCCESS 0
 #define DEVICE_NAME "pkd"   /* Dev name as it appears in /proc/devices   */
 #define BUF_LEN 80      /* Max length of the message from the device */
+
+/* 
+ * Global variables are declared as static, so are global within the file. 
+ */
+
+static int Major;       /* Major number assigned to our device driver */
+static int Device_Open = 0; /* Is device open?  
+                 * Used to prevent multiple access to device */
+static char msg[BUF_LEN];   /* The msg the device will give when asked */
+static char *msg_Ptr;
+
+static struct file_operations fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
+};
