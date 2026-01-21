dd674482418092c5a80b50b69c5d7784ac1793d2
Prav Dubey <prav@pravdubey1.com>
2026-01-21 13:19:29 -0800

    Added pkd_init fn

--- pkd.c
+++ pkd.c
@@ -45,3 +45,26 @@ static struct file_operations fops = {
     .open = device_open,
     .release = device_release
 };
+
+/*
+ * This function is called when the module is loaded
+ */
+static int __init pkd_init(void)
+{
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (Major < 0) {
+      printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+      return Major;
+    }
+
+    printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
+    printk(KERN_INFO "the driver, create a dev file with\n");
+    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+    printk(KERN_INFO "the device file.\n");
+    printk(KERN_INFO "Remove the device file and module when done.\n");
+
+    return SUCCESS;
+}
+
